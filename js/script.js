34e490a4f2f407943a02a3c9066185bb6cb618f6
Minseo Jang <gs24091@gs.hs.kr>
2025-05-11 18:50:25 +0900

    13

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// 모바일 메뉴 토글
-const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.main-nav');
-navToggle.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
